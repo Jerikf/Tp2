3f756c9c35f07a0c6658f38f73d3bbb29689c868
Erik <jerikfc@gmail.com>
2021-11-04 11:06:18 -0300

    Creacion del main- prueba de uso de los edificios y el vector

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "./entidades/firmas/Edificio.h"
+#include "./entidades/firmas/Escuela.h"
+#include "./entidades/firmas/Aserradero.h"
+#include "./entidades/firmas/Mina.h"
+#include "./estructura/Vect.h"
+#include <iostream>
+
+int main(){
+    //Edificio edificio("escuelaa Prueba", 5, 56, 5, 3);
+    /*Escuela escuela("escuelaa Prueba", 5, 56, 5, 3);
+    escuela.mostrar();
+    Edificio* ptr = new Escuela("madre admirable", 5, 6, 3, 2);
+    ptr->mostrar();
+    delete ptr;*/
+
+    //--------------------------------- otra prueba------------------------
+    Vect<Edificio> edificios;
+    Edificio* escuela = new Escuela("san josé",1,1,1,1);
+    Edificio* escuela2 = new Escuela("lavalle",2,2,2,2);
+    Edificio* edificio;
+    string nombreEdificio = "Aserradero";
+    if(nombreEdificio == "Escuela"){
+        edificio = new Escuela("Corrientes",3,3,3,3);
+    }else if(nombreEdificio == "Aserradero"){
+        edificio = new Aserradero("Cordoba",4,4,4,4);
+    }else{
+        edificio = new Mina("Santa fe",5,5,5,5);
+    }
+
+
+    edificios.agregar(escuela);
+    cout << edificios.obtenerCantidad() << endl;
+    edificios.agregar(escuela2);
+    cout << edificios.obtenerCantidad() << endl;
+    edificios.agregar(edificio);
+    cout << edificios.obtenerCantidad() << endl;
+    edificios.mostrar();
+
+
+    return 0;
+}
