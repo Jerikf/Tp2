8346a863935f4b8c10f4d329b9a61060ec519e00
Erik <jerikfc@gmail.com>
2021-11-09 16:22:23 -0300

    Se probo el uso de las las subClases de casillero

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "./entidades/firmas/Mina.h"
 #include "./estructura/Vect.h"
 #include "./accesoDatos/modelo/Datos.h"
+#include "./entidades/firmas/Casillero.h"
+#include "./entidades/firmas/Transitable.h"
+#include "./entidades/firmas/Construible.h"
+#include "./entidades/firmas/Inaccesible.h"
 #include <iostream>
 
 int main(){
@@ -84,7 +88,7 @@ int main(){
 
     //------------------------------------------------------------
     
-    
+    /* ---------SE PROBÒ EL USO DE CARGADO DE LOS DATOS EDIFICIOS
     Datos datos("edificios.txt", "materiales.txt");
     Vect<Edificio> edificios;
     datos.cargarDatosEdificios(&edificios);
@@ -93,6 +97,51 @@ int main(){
     datos.gurdarDatosEdificios(&edificios);
     edificios.mostrar();
 
+    -----------------------------------------------------------*/
+
+    /* -------probe el uso de los casilleros instanciando los constructores con valores además del
+    del método contruirEdificio que se redifine según sea el casillero---------*/
+    Vect<Casillero> casilleros;
+
+    Material* material1 = new Material("Piedra", 10);
+    Material* material2 = new Material("Metal", 5);
+
+    Edificio* edificio1 = new Escuela("Madre Admirable", 1,1,1,1);
+    Edificio* edificio2 = new Fabrica("Taller de Textil", 2,2,2,2);
+    Edificio* edificio3 = new Fabrica("Los pasos", 3,3,3,3);
+
+  
+    Casillero* casillero1 = new Transitable('C',material1);
+    Casillero* casillero2 = new Transitable('C',material2);
+    Casillero* casillero3 = new Construible('T',edificio1);
+    Casillero* casillero4 = new Construible('T',edificio2);
+    Casillero* casillero5 = new Inaccesible('L');
+    
+
+    casilleros.agregar(casillero1);
+    casilleros.agregar(casillero2);
+    casilleros.agregar(casillero3);
+    casilleros.agregar(casillero4);
+    casilleros.agregar(casillero5);
+
+
+    casilleros.mostrar();
+    
+    Casillero* casillero6 = new Transitable('C');
+    Casillero* casillero7 = new Construible('T');
+
+    casillero6->construirEdificio(edificio3); //acá si se debe construir
+    casillero7->construirEdificio(edificio3); //acá no se debería poder
+
+    if(casillero6->getEdificio()){
+        casillero6->getEdificio()->getNombre();
+    }
+    if(!casillero7->getEdificio()){ //osea quiere decir que está null que no lo guardó acá
+        cout << "está vacío" << endl;
+    }
+    delete casillero6;
+    delete casillero7;
+    delete edificio3; //llamo a este porque contruirEdificio te genera otro nueva instancia
 
 
     return 0;
