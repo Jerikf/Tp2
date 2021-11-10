fde0f447df8b83b8c067b992aed9579ef8588772
Erik <jerikfc@gmail.com>
2021-11-10 20:59:30 -0300

    Se probó el método contruirEdificioPorNombre con todos los casos posibles de errores que se puedan dar

--- main.cpp
+++ main.cpp
@@ -28,6 +28,38 @@ int main(){
     juego.listarEdificiosConstruidos();
     juego.listarTodosLosEdificios();
 
+    Coordenada coordenada1(0,2);
+    Coordenada coordenada2(5,8);
+    Coordenada coordenada3(0,4);
+    Coordenada coordenada4(0,2);
+    Coordenada coordenada5(7,0);
+    Coordenada coordenada6(7,7);
+    Coordenada coordenada7(0,5);
+    Coordenada coordenada8(0,0);
+    Coordenada coordenada9(100,500);
+    Coordenada coordenada10(7,8);
+    Coordenada coordenada11(6,9);
+  
+
+
+
+    juego.construirEdificioPorNombre("mina", coordenada1); // LIMITE ALCANZADO
+    juego.construirEdificioPorNombre("planta electrica", coordenada2); // OK
+    juego.construirEdificioPorNombre("escuela",coordenada3); //   YA ESTÁ OCUPADO
+    juego.construirEdificioPorNombre("aserradero",coordenada4); // OK
+    juego.construirEdificioPorNombre("obelisco", coordenada5); //ERROR NOMBRE --> no está en la lista de edificios cargados
+    juego.construirEdificioPorNombre("aserradero",coordenada6); // OK
+    juego.construirEdificioPorNombre("aserradero",coordenada7); // ERROR--> Coordenada de un CAMINO
+    juego.construirEdificioPorNombre("aserradero",coordenada8); // ERROR--> Coordenada de un LAGO
+    juego.construirEdificioPorNombre("aserradero", coordenada9); // ERROR--> Coordeanda fuera de RANGO
+    juego.construirEdificioPorNombre("escuela", coordenada10); // OK
+    juego.construirEdificioPorNombre("escuela", coordenada11); // ERROR ---> FALTA MATERIAL(MADERA)
+
+
+    juego.listarTodosLosEdificios();
+    juego.listarEdificiosConstruidos();
+    juego.mostrarInventario();
+
 
     return 0;
 }
