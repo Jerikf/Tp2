f2fa4ea483425434ba1d712ce498c1bdf0601d38
Erik <jerikfc@gmail.com>
2021-11-10 12:10:05 -0300

    Se agrego el contructor y destructor de la clase Recursos

--- recursos/implementacion/Recursos.cpp
+++ recursos/implementacion/Recursos.cpp
@@ -1,6 +1,9 @@
 #include "../firmas/Recursos.h"
 #include <iostream>
 
+Recurso::Recurso(){}
+Recurso::~Recurso(){}
+
 string Recurso::convertirAMayuscula(string cadena){
     string cadenaNueva = "";
 	for(int pos = 0; pos < (int)cadena.size(); pos++)
